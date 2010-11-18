33ff9e2d3d58deb37103972f0ce36e6fe2d1abcf
Gustav Ernberg <github@minpunkt.se>
2010-11-18 01:09:31 +0100

    Ett fönster

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+import window.Window;
+
+public class Main {
+    public static void main(String[] args){
+        Main m = new Main();
+        m.start();
+    }
+    public void start(){
+        Window w = new Window();
+    }
+}
---
+++ src/window/Board.java
@@ -0,0 +1,19 @@
+package window;
+
+
+import javax.swing.JPanel;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gustav
+ */
+public class Board extends JPanel{
+    public Board(){
+        
+    }
+}
