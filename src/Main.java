d3daec5d1040987fc8cf7a997f5f977271cb407c
Gustav Ernberg <gustav@ubuntu.ubuntu-domain>
2010-12-14 15:50:01 +0100

    Lade till filer

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+import graphics.Window;
+import objects.UserController;
+
+
+public class Main {
+    public static void main(String[] args){
+        Main m = new Main();
+        m.start();
+    }
+    public void start(){
+        Window window = new Window();
+        UserController userController = new UserController(window);
+        Coordinator coordinator = new Coordinator(window, userController);
+
+        while(true)
+        {
+            coordinator.update();
+            try {
+                Thread.sleep(15);
+            } catch (InterruptedException ex) { }
+        }
+    }
+}
---
+++ src/objects/Stealable.java
@@ -0,0 +1,7 @@
+package objects;
+public interface Stealable {
+    /**
+     * Vad som ska hända med objektet när det stjäls, ex. ska ett larm gå?
+     */
+    public void stealAction();
+}
